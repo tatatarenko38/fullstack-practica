ccf3ff8f9cfb6686f117dcbe27b646943c0acb47
Татьяна Татаренко <tatatatarenko38@gmail.com>
2024-07-16 10:21:05 +0300

    use-reducer

--- front/src/App.js
+++ front/src/App.js
@@ -1,66 +1,170 @@
-import { React, useState, useEffect } from "react";
+import { React, useReducer } from "react";
 
 import Page from "./component/page";
+import Grid from "./component/grid";
+import Box from "./component/box";
 
-import PostList from "./container/post-list";
+// //хук редукції стану
 
-function App() {
-  //для кружечка, що прікріплений до курсору
+const LIST_ACTION_TYPE = {
+  ADD: "add",
+  DELETE: "delete",
+  SELECT: "select",
+  REVERSE: "reverse",
+};
 
-  const [position, setPosition] = useState({ x: 0, y: 0 });
+//відповідає, щоб повернути новий об'єкт стану
+function listReducer(state, action) {
+  if (!action.payload) throw new Error("need action.payload");
+  switch (action.type) {
+    case LIST_ACTION_TYPE.ADD:
+      const id = new Date().getTime();
 
-  // useEffect(() => {
-  //   function handleMove(e) {
-  //     setPosition({ x: e.clientX, y: e.clientY });
-  //   }
+      //payload - фраза, що означає данні,
+      //які приходять в нашій дії(в dispatch)
+      const newItem = { value: action.payload, id };
+      //повертаємо новий об'єкт з деструктуризацією state
+      //вказуємо властивість items, кладемо туди все,
+      //що було в state.items i action(сюди приходить нове значення,
+      //що вводиться в поле)
+      return {
+        //щоб всі значенняБ що були - збереглися
+        ...state,
+        //оновлюються тілки ці
+        items: [...state.items, newItem],
+      };
 
-  //   window.addEventListener("pointermove", handleMove);
-  //   return () => {
-  //     //при розмонтуванні прибирається
-  //     window.removeEventListener("pointermove", handleMove);
-  //   };
-  // }, []);
+    case LIST_ACTION_TYPE.DELETE:
+      const newItems = state.items.filter((item) => item.id !== action.payload);
 
-  // або
+      return {
+        ...state,
+        items: newItems,
+      };
 
-  useWindowListener("pointermove", (e) => {
-    setPosition({ x: e.clientX, y: e.clientY });
-  });
+    case LIST_ACTION_TYPE.SELECT:
+      return {
+        //повертаємо минулий state
+        ...state,
+        //та властивість selectedId, якщо === то до цього був обраний
+        //цей елемент і ми ще раз на нього натискаємо, то null -
+        //тобто прибиралося, що елемент обраний(відбувається перемикання
+        //активного елемента)
+        selectedId: action.payload === state.selectedId ? null : action.payload,
+      };
 
-  return (
-    <Page>
-      <PostList />
+    case LIST_ACTION_TYPE.REVERSE:
+      return {
+        ...state,
+        items: state.items.reverse(),
+      };
 
-      {/* стилі для кружечка */}
-      <div
-        style={{
-          position: "absolute",
-          backgroundColor: "pink",
-          borderRadius: "50%",
-          opacity: 0.6,
-          transform: `translate(${position.x}px, ${position.y}px)`,
-          pointerEvents: "none",
-          left: -20,
-          top: -20,
-          width: 40,
-          height: 40,
-        }}
-      />
-    </Page>
-  );
+    default:
+      //повертаємо ті самі значення
+      return { ...state };
+  }
 }
 
-//власна функція (загальна) для  використання
-// в різних компонентах
-//  спрцьовує коли eventType,listener - змінюються
-export function useWindowListener(eventType, listener) {
-  useEffect(() => {
-    window.addEventListener(eventType, listener);
+const initState = { items: [] };
 
-    return () => {
-      window.removeEventListener(eventType, listener);
-    };
-  }, [eventType, listener]);
+function App() {
+  //   const [state, dispatch] = useReducer(listReducer, initState);
+
+  //використання init в хуке редукції стану
+  //додаємо початкові данні без переписання initState
+  //це локально прив'язано до конкретного компонента
+  const init = (state) => {
+    if (state.items && state.items.length === 0) {
+      return {
+        ...state,
+        items: [{ id: 432312, value: "first item" }],
+      };
+    } else {
+      return state;
+    }
+  };
+
+  const [state, dispatch] = useReducer(listReducer, initState, init);
+
+  const handleAddItem = (e) => {
+    // для зручності, щоб не писати e.target
+    const { value } = e.target;
+
+    //trimm() прибирає зайві пробіли(щоб не було значення одні пробіли)
+    if (value.trim() === "") return null;
+    // console.log(e.target.value);
+    dispatch({ type: LIST_ACTION_TYPE.ADD, payload: value });
+
+    //зкидаємо значення в полі
+    e.target.value = "";
+  };
+
+  // для кнопки "видалити"
+  const handleRemoveItem = (id) =>
+    dispatch({ type: LIST_ACTION_TYPE.DELETE, payload: id });
+
+  //для вибору активного елемента (payload: id - id елемента,
+  //який треба обрати
+  const handleSelectItem = (id) =>
+    dispatch({ type: LIST_ACTION_TYPE.SELECT, payload: id });
+
+  // змінити порядок
+  const handleReverseItems = () => dispatch({ type: LIST_ACTION_TYPE.REVERSE });
+  return (
+    <Page>
+      <Grid>
+        <Box>
+          <Grid>
+            <h1>Список елементів</h1>
+
+            <ul>
+              <Grid>
+                {state.items.map(({ value, id }) => (
+                  <li onClick={() => handleSelectItem(id)} key={id}>
+                    <Box
+                      style={{
+                        borderColor:
+                          state.selectedId === id ? "blue" : "#e6e6e6",
+                      }}
+                    >
+                      <Grid>
+                        <span>{value}</span>
+                        <Box>
+                          <button
+                            onClick={(e) => {
+                              // щоб не спливала подія, бо в нас є ще
+                              //один onClick y selecty
+                              e.stopPropagation();
+                              handleRemoveItem(id);
+                            }}
+                          >
+                            Видалити
+                          </button>
+                        </Box>
+                      </Grid>
+                    </Box>
+                  </li>
+                ))}
+              </Grid>
+            </ul>
+          </Grid>
+        </Box>
+
+        <Box>
+          <input
+            // подія, коли ми виходимо з input в браузері(змінюємо фокус)
+            onBlur={handleAddItem}
+            type="text"
+            placeholder="Введіть новий елемент"
+          />
+        </Box>
+
+        <Box>
+          <button onClick={handleReverseItems}>Змінити порядок</button>
+        </Box>
+      </Grid>
+    </Page>
+  );
 }
 
 export default App;
